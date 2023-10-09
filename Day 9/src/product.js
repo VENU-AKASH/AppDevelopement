183b8b743df470af1de4162706cae3fceb1b6d5e
VENU-AKASH <104961295+VENU-AKASH@users.noreply.github.com>
2023-10-09 12:22:32 +0530

    Add files via upload

---
+++ Day 9/src/product.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/R (2).jpeg";
+import product2 from "./assets/products/HS.jpeg";
+import product3 from "./assets/products/BG.jpeg";
+import product4 from "./assets/products/BD.jpg";
+import product5 from "./assets/products/T.jpg";
+import product6 from "./assets/products/TT.jpeg";
+import product7 from "./assets/products/TB.jpeg";
+import product8 from "./assets/products/BD.jpg";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Lego Starwars",
+    price: 999.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "Hotwheels Skyline",
+    price: 1999.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Electric Airsoft bullet Gun",
+    price: 699.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Barbie",
+    price: 228.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Transformers",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Toy Train",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Teddy Bear",
+    price: 120.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Baby Toys",
+    price: 40.0,
+    productImage: product8,
+  },
+];
